beccf176b728105214a9d2722e3e954db99514d7
Karel Kozlík <kozlik@kufr.cz>
2022-03-04 09:15:51 +0100

    Make deep copy of the data to not modify original object by the escape function #6066

--- src/extensions/editable/bootstrap-table-editable.js
+++ src/extensions/editable/bootstrap-table-editable.js
@@ -171,7 +171,7 @@ $.BootstrapTable = class extends $.BootstrapTable {
   }
 
   getData (params) {
-    const data = super.getData(params)
+    const data = structuredClone(super.getData(params))
 
     if (params && params.escape) {
       for (const row of data) {
